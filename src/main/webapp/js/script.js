4edc37907182f108dbe18e6ea012ef843e5665b0
namitagrawal2001 <namitagrawal58@gmail.com>
2025-09-17 16:53:16 +0530

    updates

--- src/main/webapp/js/script.js
+++ src/main/webapp/js/script.js
@@ -1,1 +1,16 @@
-console.log("Car Rental App Loaded");
+// Simple login simulation
+document.addEventListener("DOMContentLoaded", function(){
+    const loginForm = document.getElementById("loginForm");
+    if(loginForm){
+        loginForm.addEventListener("submit", function(e){
+            e.preventDefault();
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+            if(username === "customer" && password === "1234"){
+                window.location.href = "index.html";
+            } else {
+                document.getElementById("errorMsg").innerText = "Invalid username or password!";
+            }
+        });
+    }
+});
